3b81444a28bd815daa5fad78aed49db0665cab22
lai2-01 <lai2-01@itismeucci.com>
2021-09-28 09:56:46 +0200

    primo commit

---
+++ src/main/java/it/alessandro_di_vincenzo/App.java
@@ -0,0 +1,13 @@
+package it.alessandro_di_vincenzo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
